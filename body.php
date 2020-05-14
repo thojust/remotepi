7932fc0252a73fe2a2a913cbf064eb033be312f5
Justin <thojust@gmail.com>
2020-05-14 16:25:42 -0400

    playin around

--- body.php
+++ body.php
@@ -1,15 +1,6 @@
+<html>
+<head></head>
+<body>
+</body>
 
-<?php 
-  
-  // Load an image from jpeg URL 
- imagecreatefromjpeg( 
-  'https://www.thetelegram.com/media/photologue/photos/cache/GS__4_-_rainbow_Brian_Hay_Granville_Ferry_NS_large.jpg'); 
-    
-  // View the loaded image in browser using imagejpeg() function 
-  header('Content-type: image/jpg');   
-  // Decrease the quality of image to 2 
-  imagejpeg($im);  
-  echo "did it work";
- 
-  ?> 
-  
+</html>
